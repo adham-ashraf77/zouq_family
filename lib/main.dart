be34792741297a5fa96d7a04f1afbb2b6aeb8d9c
KiD1905 <kid1905@hotmail.com>
2020-04-14 14:05:07 +0200

    created filter chip widget switched choice chips with filter chips

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+
 import 'package:zouqadmin/theme/common.dart';
 import 'package:zouqadmin/pages/ordersViewPage.dart';
 import './pages/ordersPage.dart';
