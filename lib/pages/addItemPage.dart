b51b112a3dd8d6eeb5fa0195b3ab8df55b6c681e
rami sadek <ramyahmedsadek@gmail.com>
2020-04-21 14:20:57 +0200

    POC of upload list of photos<Assets>

--- lib/pages/addItemPage.dart
+++ lib/pages/addItemPage.dart
@@ -1,4 +1,11 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:multi_image_picker/multi_image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:zouqadmin/theme/common.dart';
 
 class AddItemPage extends StatefulWidget {
@@ -8,6 +15,127 @@ class AddItemPage extends StatefulWidget {
 
 class _AddItemPageState extends State<AddItemPage> {
   String _dropdownValue;
+  List<File> listOfImages = List<File>();
+  List<Asset> images = List<Asset>();
+  bool photosIsEmpty = false;
+
+  Future<void> loadAssets() async {
+    List<Asset> resultList = List<Asset>();
+//    if(file!=null && file.isNotEmpty)
+//      file.clear();
+
+    String error = 'No Error Dectected';
+
+    Future<File> getImageFileFromAssets(ByteData bytedata, String name) async {
+      print('hi');
+      final byteData = bytedata;
+
+      final file = File('${(await getTemporaryDirectory()).path}/$name');
+      await file.writeAsBytes(byteData.buffer.asUint8List(
+          byteData.offsetInBytes, byteData.lengthInBytes
+      ),
+      );
+      print('bye');
+      return file;
+    }
+
+
+    try {
+      resultList = await MultiImagePicker.pickImages(
+        maxImages: 5,
+        enableCamera: true,
+        selectedAssets: images,
+        cupertinoOptions: CupertinoOptions(takePhotoIcon: "chat"),
+        materialOptions: MaterialOptions(
+          actionBarColor: "#abcdef",
+          actionBarTitle: "Example App",
+          allViewTitle: "All Photos",
+          useDetailsView: false,
+          selectCircleStrokeColor: "#000000",
+        ),
+      );
+    } on Exception catch (e) {
+      error = e.toString();
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      photosIsEmpty = false;
+      images = resultList;
+    });
+    print('hi');
+    print(images.length);
+    images.forEach((image) async {
+//      images.first.requestOriginal(quality: 100).then((value){
+//
+//        print('yoooooooooooooo${value.buffer}');
+//      });
+
+      listOfImages.add(
+          await getImageFileFromAssets(
+              await image.requestOriginal(quality: 100), image.name)
+      );
+      print(image.name);
+      print('fileLength----->${listOfImages.length}');
+    });
+
+    print('hi');
+    //print('--------->${file.first.path}');
+  }
+
+  addItem(List<File> listOfPhotos) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String token = prefs.getString("token") ?? "";
+    print('token: $token');
+    List<MultipartFile> something = List();
+
+    listOfPhotos.forEach((photo) async {
+      something.add(MultipartFile.fromFileSync("${photo.path}"));
+      print(listOfPhotos.length);
+      print(something.length);
+      if (listOfPhotos.length == something.length) {
+        print('hi ^^');
+        FormData _formData = FormData.fromMap({
+          "name": "test1",
+          "description": "descriptiondescriptiondescription1111",
+          "price": "100",
+          "images": something,
+          "category_id": 2,
+        });
+        try {
+          print('--------------------> addItemFile: ${_formData.files}');
+          Response response;
+          if (token.isNotEmpty)
+            {
+              print('before response');
+              response = await Dio().post("http://api.dhuqapp.com/api/family/products",
+                  data: _formData,
+                  options: Options(
+                    headers: {HttpHeaders.authorizationHeader: "Bearer $token"},
+                  ));
+              print('after response');
+              print(response.data);
+            }
+          if (response.statusCode == 200) {
+            return "success";
+          } else {
+            print(response.data);
+            return "something is wrong";
+          }
+        } on DioError catch (e) {
+          print('errooooooooorrrrrrrrr');
+          if (e.response == null) return "connection time out";
+          print(e.response.data);
+        }
+      }
+      return null;
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -82,6 +210,97 @@ class _AddItemPageState extends State<AddItemPage> {
             SizedBox(
               height: 10,
             ),
+
+
+
+
+            Padding(
+                padding: EdgeInsets.only(top: MediaQuery
+                    .of(context)
+                    .padding
+                    .top),
+                child: SizedBox(
+                  height: MediaQuery
+                      .of(context)
+                      .size
+                      .height * 0.15,
+                  child: Directionality(
+                      textDirection: TextDirection.rtl,
+                      child: images.length == 0 ?
+                      Container(
+                        width: MediaQuery
+                            .of(context)
+                            .size
+                            .width * 0.35,
+                        margin: EdgeInsets.only(bottom: 10),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Colors.green, width: 2),
+                            borderRadius: BorderRadius.all(
+                                Radius.circular(20))
+                        ),
+                        alignment: Alignment.center,
+                        child: Icon(Icons.camera_alt),
+                      )
+                          :
+
+                      ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: images.length,
+                          itemBuilder: (
+                                  (BuildContext context, int index) {
+                                Asset asset = images[index];
+                                return AssetThumb(
+                                  asset: asset,
+                                  width: 300,
+                                  height: 300,
+                                );
+                              }))
+                  ),
+
+                )
+            ),
+            InkWell(
+              onTap: () {
+                if (listOfImages != null && listOfImages.isNotEmpty) {
+                  listOfImages.clear();
+                }
+                loadAssets();
+              },
+              child: Container(
+                width: MediaQuery
+                    .of(context)
+                    .size
+                    .width * 0.35,
+                height: MediaQuery
+                    .of(context)
+                    .size
+                    .height * 0.07,
+                margin: EdgeInsets.only(top: 10),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.green, width: 2),
+                    borderRadius: BorderRadius.all(Radius.circular(20))
+                ),
+                alignment: Alignment.center,
+                child: Text(
+                  "choose a pic",),
+              ),
+            ),
+
+
+            RaisedButton(
+              onPressed: (){
+                addItem(listOfImages);
+              },
+            ),
+
+
+
+
+
+
+
+
             Container(
               width: MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.height * 0.1,
