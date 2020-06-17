533350863498b799634077c352f7c8e6052fc60b
omar sherif <omar.sherifs@live.com>
2020-06-17 15:21:27 +0200

    add image picker

--- lib/utils/helpers.dart
+++ lib/utils/helpers.dart
@@ -1,19 +1,80 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-// import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:zouqadmin/I10n/app_localizations.dart';
 
-////////////////////////////////////////////////
-/// GPS and location helpers
-////////////////////////////////////////////////
+File selectedImg;
+ImageSource imageSource;
 
-// Future<double> getDistanceBetween(LatLng firstPoint, LatLng secondPoint) async {
-//   double distanceInMeters = await Geolocator().distanceBetween(
-//       firstPoint.latitude,
-//       firstPoint.longitude,
-//       secondPoint.latitude,
-//       secondPoint.longitude);
-//   return distanceInMeters;
-// }
+getImageSource(BuildContext context) async {
+  imageSource = await showDialog<ImageSource>(
+      context: context,
+      builder: (context) => AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(15))),
+            title: Text(
+                AppLocalizations.of(context).translate('imaPickerDialogTitle')),
+            actions: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  MaterialButton(
+                    child: Row(
+                      children: <Widget>[
+                        Icon(Icons.camera_alt),
+                        Text(AppLocalizations.of(context).translate('camera')),
+                      ],
+                    ),
+                    onPressed: () => Navigator.pop(context, ImageSource.camera),
+                  ),
+                  MaterialButton(
+                    child: Row(
+                      children: <Widget>[
+                        Icon(Icons.photo),
+                        Text(AppLocalizations.of(context).translate('gallery')),
+                      ],
+                    ),
+                    onPressed: () =>
+                        Navigator.pop(context, ImageSource.gallery),
+                  )
+                ],
+              ),
+            ],
+          ));
+}
+
+Future <File> getImage(ImageSource src) async {
+  File selectedImg = await ImagePicker.pickImage(source: src);
+  selectedImg = await ImageCropper.cropImage(
+      sourcePath: selectedImg.path,
+      aspectRatioPresets: [
+        CropAspectRatioPreset.square,
+        CropAspectRatioPreset.ratio3x2,
+        CropAspectRatioPreset.original,
+        CropAspectRatioPreset.ratio4x3,
+        CropAspectRatioPreset.ratio16x9
+      ],
+      androidUiSettings: AndroidUiSettings(
+          toolbarColor: Colors.purple,
+          activeControlsWidgetColor: Colors.purple,
+          toolbarWidgetColor: Colors.white,
+          initAspectRatio: CropAspectRatioPreset.original,
+          lockAspectRatio: false),
+      iosUiSettings: IOSUiSettings(
+        minimumAspectRatio: 1.0,
+      ));
+  return selectedImg;
+}
+
+ selectImg(BuildContext context) async {
+  File file;
+  await getImageSource(context);
+  file = await getImage(imageSource);
+  return file;
+}
 
 Future<Position> getCurrentLocation() async {
   Position position = await Geolocator()
